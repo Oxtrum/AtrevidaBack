c1791f029a8a82b74f12e7afbd248f28ea0a3c41
PHansLM <pablito.hans@hotmail.com>
2026-05-23 14:15:13 -0400

    feat: Recovered CORS origins config

--- router/router.go
+++ router/router.go
@@ -17,7 +17,14 @@ import (
 func Setup(h *handlers.Container, repo *sheetsrepo.ReservasRepo) *gin.Engine {
 	r := gin.Default()
 	r.Use(cors.New(cors.Config{
-		AllowAllOrigins: true,
+		AllowOrigins: []string{
+			"http://localhost:3000",
+			"http://127.0.0.1:3000",
+			"http://localhost:5173",
+			"http://127.0.0.1:5173",
+			"http://localhost:4173",
+			"http://127.0.0.1:4173",
+		},
 		AllowMethods: []string{
 			http.MethodGet,
 			http.MethodPost,
