e758f52610b8f839a3b9fb80f5bb090c776e69d6
Diego Grajeda <grajedadiego1@gmail.com>
2026-05-27 17:50:06 -0400

    Separar construccion de la app

---
+++ app/app.go
@@ -0,0 +1,68 @@
+package app
+
+import (
+	"atrevida-agenda-api/config"
+	"atrevida-agenda-api/db"
+	"atrevida-agenda-api/docs"
+	"atrevida-agenda-api/handlers"
+	pgsqlrepo "atrevida-agenda-api/repositories/pgsql"
+	"atrevida-agenda-api/router"
+	"atrevida-agenda-api/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	Name    = "Atrevida Fit - Agenda API"
+	Version = "v0.1"
+)
+
+// Build initializes config, dependencies, handlers and the HTTP router.
+func Build() (*gin.Engine, error) {
+	config.Load()
+	configureSwagger()
+
+	pgDB, err := db.Connect(config.App)
+	if err != nil {
+		return nil, err
+	}
+
+	categoriasPGRepo := pgsqlrepo.NewCategoriasRepo(pgDB)
+	clientesPGRepo := pgsqlrepo.NewClientesRepo(pgDB)
+	localesHorariosPGRepo := pgsqlrepo.NewLocalesHorariosRepo(pgDB)
+	serviciosPGRepo := pgsqlrepo.NewServiciosRepo(pgDB)
+	combosPGRepo := pgsqlrepo.NewCombosRepo(pgDB)
+	comboServiciosPGRepo := pgsqlrepo.NewComboServiciosRepo(pgDB)
+	reservasPGRepo := pgsqlrepo.NewReservasRepo(pgDB)
+	localesPGRepo := pgsqlrepo.NewLocalesRepo(pgDB)
+
+	categoriasPGService := services.NewCategoriasService(categoriasPGRepo)
+	clientesPGService := services.NewClientesService(clientesPGRepo)
+	localesHorariosPGService := services.NewLocalesHorariosService(localesHorariosPGRepo)
+	serviciosPGService := services.NewServiciosPGService(serviciosPGRepo)
+	combosPGService := services.NewCombosService(combosPGRepo)
+	comboServiciosPGService := services.NewComboServiciosService(comboServiciosPGRepo)
+	reservasPGService := services.NewReservasPGService(reservasPGRepo, serviciosPGRepo)
+	localesPGService := services.NewLocalesService(localesPGRepo)
+
+	h := handlers.NewContainer(
+		categoriasPGService,
+		clientesPGService,
+		localesHorariosPGService,
+		serviciosPGService,
+		combosPGService,
+		comboServiciosPGService,
+		reservasPGService,
+		localesPGService,
+	)
+
+	return router.Setup(h), nil
+}
+
+func configureSwagger() {
+	docs.SwaggerInfo.Title = Name
+	docs.SwaggerInfo.Version = Version
+	docs.SwaggerInfo.Description = "API REST para reservas, servicios, combos y administracion de Atrevida Fit."
+	docs.SwaggerInfo.BasePath = "/"
+	docs.SwaggerInfo.Schemes = []string{"http", "https"}
+}
--- main.go
+++ main.go
@@ -1,13 +1,7 @@
 package main
 
 import (
-	"atrevida-agenda-api/config"
-	"atrevida-agenda-api/db"
-	"atrevida-agenda-api/docs"
-	"atrevida-agenda-api/handlers"
-	pgsqlrepo "atrevida-agenda-api/repositories/pgsql"
-	"atrevida-agenda-api/router"
-	"atrevida-agenda-api/services"
+	"atrevida-agenda-api/app"
 )
 
 //go:generate go run github.com/swaggo/swag/cmd/swag@v1.16.6 init -g main.go -o docs
@@ -18,52 +12,14 @@ import (
 // @BasePath /
 // @schemes http https
 func main() {
-	config.Load()
-
-	versionString := "v0.1"
-	apiName := "Atrevida Fit - Agenda API" + "(" + versionString + ")"
-	docs.SwaggerInfo.Title = "Atrevida Fit - Agenda API"
-	docs.SwaggerInfo.Version = versionString
-	docs.SwaggerInfo.Description = "API REST para reservas, servicios, combos y administracion de Atrevida Fit."
-	docs.SwaggerInfo.BasePath = "/"
-	docs.SwaggerInfo.Schemes = []string{"http", "https"}
-
-	pgDB, err := db.Connect(config.App)
+	routerAtrevida, err := app.Build()
 	if err != nil {
 		panic(err)
 	}
 
-	categoriasPGRepo := pgsqlrepo.NewCategoriasRepo(pgDB)
-	clientesPGRepo := pgsqlrepo.NewClientesRepo(pgDB)
-	localesHorariosPGRepo := pgsqlrepo.NewLocalesHorariosRepo(pgDB)
-	serviciosPGRepo := pgsqlrepo.NewServiciosRepo(pgDB)
-	combosPGRepo := pgsqlrepo.NewCombosRepo(pgDB)
-	comboServiciosPGRepo := pgsqlrepo.NewComboServiciosRepo(pgDB)
-	reservasPGRepo := pgsqlrepo.NewReservasRepo(pgDB)
-	localesPGRepo := pgsqlrepo.NewLocalesRepo(pgDB)
+	println(app.Name + "(" + app.Version + ")\n" + "Running")
 
-	categoriasPGService := services.NewCategoriasService(categoriasPGRepo)
-	clientesPGService := services.NewClientesService(clientesPGRepo)
-	localesHorariosPGService := services.NewLocalesHorariosService(localesHorariosPGRepo)
-	serviciosPGService := services.NewServiciosPGService(serviciosPGRepo)
-	combosPGService := services.NewCombosService(combosPGRepo)
-	comboServiciosPGService := services.NewComboServiciosService(comboServiciosPGRepo)
-	reservasPGService := services.NewReservasPGService(reservasPGRepo, serviciosPGRepo)
-	localesPGService := services.NewLocalesService(localesPGRepo)
-
-	h := handlers.NewContainer(
-		categoriasPGService,
-		clientesPGService,
-		localesHorariosPGService,
-		serviciosPGService,
-		combosPGService,
-		comboServiciosPGService,
-		reservasPGService,
-		localesPGService,
-	)
-
-	println(apiName + "\n" + "Running")
-
-	routerAtrevida := router.Setup(h)
-	routerAtrevida.Run(":8080")
+	if err := routerAtrevida.Run(":8080"); err != nil {
+		panic(err)
+	}
 }
