ee6c686468e0bdd2d810ebe4d6a0a1c70d07c129
PHansLM <pablito.hans@hotmail.com>
2026-05-28 14:32:06 -0400

    migrate: Usuarios table

---
+++ migrations/000020_create_usuarios.down.sql
@@ -0,0 +1,2 @@
+DROP INDEX IF EXISTS uq_usuarios_username_activo;
+DROP TABLE IF EXISTS usuarios;
---
+++ migrations/000020_create_usuarios.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS usuarios (
+    id       SERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    activo   BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS uq_usuarios_username_activo
+    ON usuarios(username)
+    WHERE activo = TRUE;
